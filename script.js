88ee751107e7ba6ccef74657ac02bd8b511a3a71
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-08-25 06:42:55 -0700

    refactored some codes

--- script.js
+++ script.js
@@ -28,4 +28,8 @@ window.onscroll = () =>{
     })
 
     let header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
