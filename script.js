765920691766e9662e0378821585255df026fc61
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-08-29 04:29:25 -0700

    added some new changes

--- script.js
+++ script.js
@@ -33,3 +33,27 @@ window.onscroll = () =>{
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+
+ScrollReveal({
+    
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1 .about-img', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'top'})
+
+
+
+
+const typed = new Typed('.multiple-text',{
+    strings: ['Security Analyst', 'Lawyer', 'Corporate Secretary'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
